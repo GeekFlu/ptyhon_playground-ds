54a39bfc6aa7b711ba27569bd61b81e4253fca6f
Spike Storm <software.j2ee@gmail.com>
2020-11-26 22:34:31 -0800

    Part B

--- P0/Task4.py
+++ P0/Task4.py
@@ -25,3 +25,29 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+if __name__ == "__main__":
+    call_receivers = set()
+    text_senders = set()
+    text_receivers = set()
+    tkm_set = set()
+    for call_record in calls:
+        call_receivers.add(call_record[1])
+
+    for text_record in texts:
+        text_senders.add(text_record[0])
+        text_receivers.add(text_record[1])
+
+    for call_record in calls:
+        sender = call_record[0]
+        if sender in call_receivers:
+            continue
+        if sender in text_senders:
+            continue
+        if sender in text_receivers:
+            continue
+        tkm_set.add(sender)
+    tkm_list = list(tkm_set)
+    tkm_list.sort()
+    print(f"These numbers could be telemarketers: ")
+    for telemarketer in tkm_list:
+        print(telemarketer)
