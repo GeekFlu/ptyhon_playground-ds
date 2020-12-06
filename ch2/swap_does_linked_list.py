e284c706ce0ffa62bfdc295a5a602e061533bd0b
Spike Storm <software.j2ee@gmail.com>
2020-12-05 21:17:20 -0800

    method definitions

--- ch2/swap_does_linked_list.py
+++ ch2/swap_does_linked_list.py
@@ -1,5 +1,6 @@
 from ch2.even_after_odd_nodes import print_linked_list
 from ch2.generate_numbers import generate_test_data
+from ch2.even_after_odd_nodes import create_linked_list
 
 """
 Given a linked list, swap the two nodes present at position i and j, assuming 0 <= i <= j. The positions are based on 0-based indexing.
@@ -31,7 +32,7 @@ Algorithm:
         temp = CR.next
         CR.next = CL.next
         PR.next = CL
-        CL = temp
+        CL.next = temp
 
     2) If head needs to ber swapped
 
@@ -57,7 +58,42 @@ def swap_nodes(head, left_index, right_index):
 
     Do not create a new linked list
     """
+    if head is None:
+        return None
+
+    if left_index > right_index:
+        raise ValueError("Left index is grater than Right index")
+
+    previous_left = None
+    current_left = None
+    previous_right = None
+    current_right = None
+    temporal_head = None
+    current = head
+    position = 0
+    # we go to pl
+    while current is not None:
+        if position <= left_index - 1:
+            previous_left = current
+            current_left = current.next
+
+        if position <= right_index - 1:
+            previous_right = current
+            current_right = current.next
+
+        current = current.next
+        position += 1
+    # Swapping
+    previous_left.next = current_right
+    temp = current_right.next
+    current_right.next = current_left.next
+    previous_right.next = current_left
+    current_left.next = temp
+    return head
 
 
 if __name__ == "__main__":
-    pass
+    ll = create_linked_list([3, 4, 5, 2, 6, 1, 9])
+    ll_swapped = swap_nodes(ll, 2, 5)
+    print_linked_list(ll)
+    print_linked_list(ll_swapped)
