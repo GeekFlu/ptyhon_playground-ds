5c18bd250a3b15c1f5d0e2444c7323da66deaa65
Spike Storm <software.j2ee@gmail.com>
2020-12-08 08:20:43 -0800

    equation checker

--- ch2/stacks_and_queues/stack_with_linked_lists.py
+++ ch2/stacks_and_queues/stack_with_linked_lists.py
@@ -1,5 +1,64 @@
 from datastructures.StackLL import Stack
 
+"""
+Reverse Polish Notation
+
+Reverse Polish notation, also referred to as Polish postfix notation is a way of laying out operators and operands.
+
+When making mathematical expressions, we typically put arithmetic operators (like +, -, *, and /) between operands. For example: 5 + 7 - 3 * 8
+
+However, in Reverse Polish Notation, the operators come after the operands. For example: 3 1 + 4 *
+
+The above expression would be evaluated as (3 + 1) * 4 = 16
+
+The goal of this exercise is to create a function that does the following:
+
+    Given a postfix expression as input, evaluate and return the correct final answer.
+
+Note: In Python 3, the division operator / is used to perform float division. So for this problem, you should use int() after every division to convert the answer to an integer.
+
+"""
+
+
+def evaluate_post_fix(input_list):
+    """
+    Evaluate the postfix expression to find the answer
+
+    Args:
+       input_list(list): List containing the postfix expression
+    Returns:
+       int: Postfix expression solution
+    """
+    s = Stack()
+    if input_list is None:
+        return None
+    if len(input_list) <= 0:
+        return None
+    for num_operand in input_list:
+        if num_operand == '*':
+            second = s.pop()
+            first = s.pop()
+            output = first * second
+            s.push(output)
+        elif num_operand == '/':
+            second = s.pop()
+            first = s.pop()
+            output = int(first / second)
+            s.push(output)
+        elif num_operand == '+':
+            second = s.pop()
+            first = s.pop()
+            output = first + second
+            s.push(output)
+        elif num_operand == '-':
+            second = s.pop()
+            first = s.pop()
+            output = first - second
+            s.push(output)
+        else:
+            s.push(int(num_operand))
+    return s.pop()
+
 
 def equation_checker(equation):
     """
@@ -50,3 +109,7 @@ if __name__ == "__main__":
 
     print("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
     print("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
+
+    assert evaluate_post_fix(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]) == 22
+    assert evaluate_post_fix(["4", "13", "5", "/", "+"]) == 6
+    assert evaluate_post_fix(["3", "1", "+", "4", "*"]) == 16
