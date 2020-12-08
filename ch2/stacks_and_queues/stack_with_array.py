f37c9608b0ebde4c820b38628d00a3310df4b539
Spike Storm <software.j2ee@gmail.com>
2020-12-07 21:50:54 -0800

    equation checker

--- ch2/stacks_and_queues/stack_with_array.py
+++ ch2/stacks_and_queues/stack_with_array.py
@@ -1,5 +1,34 @@
 from datastructures.Stack import Stack
 
+
+def equation_checker(equation):
+    """
+    Check equation for balanced parentheses
+
+    Args:
+       equation(string): String form of equation
+    Returns:
+       bool: Return if parentheses are balanced or not
+    """
+    s = Stack()
+    if equation is None:
+        return False
+    if len(equation) <= 0:
+        return False
+
+    for letter in equation:
+        if letter == '(':
+            s.push(letter)
+        elif letter == ')':
+            if s.pop() is None:
+                return False
+
+    if s.size() == 0:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     stack = Stack()
     print(stack.is_empty())
@@ -33,3 +62,6 @@ if __name__ == "__main__":
         print(stack.arr)
     print(stack.pop())
     print(stack.arr)
+
+    print("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
+    print("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
--- ch2/stacks_and_queues/stack_with_linked_lists.py
+++ ch2/stacks_and_queues/stack_with_linked_lists.py
@@ -1,20 +1,52 @@
 from datastructures.StackLL import Stack
 
-if __name__ == "__main__":
+
+def equation_checker(equation):
+    """
+    Check equation for balanced parentheses
+
+    Args:
+       equation(string): String form of equation
+    Returns:
+       bool: Return if parentheses are balanced or not
+    """
     s = Stack()
-    s.push(1)
-    s.push(2)
-    s.push(3)
-    print(s.pop())
-    print(s.pop())
-    print(s.pop())
-    print(s.pop())
+    if equation is None:
+        return False
+    if len(equation) <= 0:
+        return False
+
+    for letter in equation:
+        if letter == '(':
+            s.push(letter)
+        elif letter == ')':
+            if s.pop() is None:
+                return False
+
+    if s.size() == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    s_ = Stack()
+    s_.push(1)
+    s_.push(2)
+    s_.push(3)
+    print(s_.pop())
+    print(s_.pop())
+    print(s_.pop())
+    print(s_.pop())
 
     for i in range(1000):
-        s.push(i)
+        s_.push(i)
 
     for i in range(999):
-        s.pop()
+        s_.pop()
 
-    assert s.size() == 1
-    print(s.pop())
+    assert s_.size() == 1
+    print(s_.pop())
+
+    print("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
+    print("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
